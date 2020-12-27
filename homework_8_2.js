34680dc2c28ad871fd03fea858056e2b4e766aee
Tetiana Kobrin <t.kobrin10gmai.com>
2020-12-27 16:24:42 +0200

    .

---
+++ homework_8_2.js
@@ -0,0 +1,32 @@
+/* Написать функцию assignObjects(obj1, obj2), 
+  которая принимает аргументами 2 объекта и возвращает новый,
+  который состоит из свойство обоих объектов (склеить). 
+  Если свойство повторяется, то взять значение второго объекта. 
+  Object.assign() - не использовать */
+
+
+function assignObjects(obj1, obj2){
+  var obj3 = obj1;
+
+  for (key in obj2) {
+    obj3[key] = obj2[key];
+  }
+  return obj3;
+}
+
+x = {
+  a: 1,
+  b: 2
+}
+  
+y = {
+  b: 5,
+  c: 8
+}
+
+console.log(assignObjects(x,y));
+
+
+
+
+
