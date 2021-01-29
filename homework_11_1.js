069ff870b47ddd62d161bbafbe5a254082d21d49
Tetiana Kobrin <t.kobrin10gmai.com>
2021-01-29 11:52:23 +0200

    .

--- homework_11_1.js
+++ homework_11_1.js
@@ -16,75 +16,111 @@
   (т.е. ВВЕРХ и ВНИЗ не работают, ПРОБЕЛ не работает)
 */
 
+
+// window.addEventListener('load', function(){
+
+//   const block = document.querySelector('.block')
+
+//     let step = 50; 
+    
+//     let stepsHorizontal = 0;
+//     let stepsVertical = 0;
+
+//     let h = 100;
+
+//     let isCrouching = false;
+
+//     function arrows (event){
+    
+//       if(event.keyCode == 37){
+//         stepsHorizontal--;
+//         block.style.left = stepsHorizontal * step + 'px';
+//       }
+//       else if(event.keyCode == 39){
+//         stepsHorizontal++;
+//         block.style.left = stepsHorizontal * step + 'px';
+//       }
+//       else if(event.keyCode == 38){
+//         if(!isCrouching){
+//           stepsVertical++;
+//           block.style.bottom = stepsVertical * step + 'px';
+//       }
+//       }
+//       else if(event.keyCode == 40){
+//         if(!isCrouching){
+//           stepsVertical--;
+//           block.style.bottom = stepsVertical * step + 'px';
+//         }
+//       }
+//       else if(event.keyCode == 32){
+//         if(!isCrouching){
+//           block.style.bottom = parseInt(block.style.bottom) + h + 'px'; 
+//           setTimeout(function(){
+//             block.style.bottom = parseInt(block.style.bottom) - h + 'px'; 
+//           }, 500);
+//         }
+//       }
+//       else if(event.keyCode == 17){
+//         isCrouching = true;
+//         block.style.height = 60 + 'px'; 
+//         block.style.width = 115 + 'px'; 
+//       }  
+//     } 
+//     document.addEventListener('keydown', arrows); 
+// });
+ 
+
+// retaked
+
 window.addEventListener('load', function(){
 
-  const block = document.querySelector('.block')
+  const block = document.querySelector('.block');
+
+    block.style.left = 200 + 'px';
+    block.style.bottom = 200 + 'px'
+    block.style.width = 100 +' px';
+    block.style.height = 100 + 'px';
+
 
     let step = 50; 
-    
-    let stepsHorizontal = 0;
-    let stepsVertical = 0;
 
     let h = 100;
 
     let isCrouching = false;
 
     function arrows (event){
-    
-      if(event.keyCode == 37){
-        stepsHorizontal--;
-        block.style.left = stepsHorizontal * step + 'px';
+
+      switch (event.keyCode){
+        case 37: // left arrow	-- 37
+          block.style.left = parseInt(block.style.left) - step + 'px';
+          break;
+        case 39: // right arrow --	39
+          block.style.left = parseInt(block.style.left) + step + 'px';
+          break;
+        case 38: // up arrow	-- 38
+          if (!isCrouching){
+            block.style.bottom = parseInt(block.style.bottom) + step + 'px';
+          }
+          break;
+        case 40: // down arrow	-- 40
+          if(!isCrouching){
+            block.style.bottom = parseInt(block.style.bottom) - step + 'px';
+          }
+          break;
+        case 32: // space -- 32
+          if(!isCrouching){
+            block.style.bottom = parseInt(block.style.bottom) + h + 'px'; 
+            setTimeout(function(){
+              block.style.bottom = parseInt(block.style.bottom) - h + 'px'; 
+            }, 500);
+          }
+          break;
+        case 17: // CTRL -- 17
+          isCrouching = true;
+          block.style.height = 60 + 'px';
+          block.style.width = 115 + 'px';
+          break
       }
-      else if(event.keyCode == 39){
-        stepsHorizontal++;
-        block.style.left = stepsHorizontal * step + 'px';
-      }
-      else if(event.keyCode == 38){
-        if(!isCrouching){
-          stepsVertical++;
-          block.style.bottom = stepsVertical * step + 'px';
-      }
-      }
-      else if(event.keyCode == 40){
-        if(!isCrouching){
-          stepsVertical--;
-          block.style.bottom = stepsVertical * step + 'px';
-        }
-      }
-      else if(event.keyCode == 32){
-        if(!isCrouching){
-          block.style.bottom = parseInt(block.style.bottom) + h + 'px'; 
-          setTimeout(function(){
-            block.style.bottom = parseInt(block.style.bottom) - h + 'px'; 
-          }, 500);
-        }
-      }
-      else if(event.keyCode == 17){
-        isCrouching = true;
-        block.style.height = 60 + 'px'; 
-        block.style.width = 115 + 'px'; 
-      }  
     } 
-    document.addEventListener('keydown', arrows); 
+  document.addEventListener('keydown', arrows); 
 });
-
-
-    // left arrow	-- 37
-    // up arrow	-- 38
-    // right arrow --	39
-    // down arrow	-- 40
-    // space -- 32
-    // CTRL -- 17
-
-
-    
-
-   
-
-      // function squat(){
-      //   if(event.keyCode == 17){
-      //     block.style.height = 60 + 'px'; //(-40%) 
-      //     block.style.width = 115 + 'px'; //(+15%)
-      //   }
-      // }
-      // document.addEventListener('keydown', squat);
